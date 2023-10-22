d50e11ac1cc2fcfc0af657973d976a56a81af0aa
Sneyder25 <fabio.zapata@unmsm.edu.pe>
2023-10-22 17:42:47 -0500

    Desarrollo Caso de Usuario 01

--- Desarrollo/PWFU/Codigo/Backend/app/routes/route.py
+++ Desarrollo/PWFU/Codigo/Backend/app/routes/route.py
@@ -1,1 +1,44 @@
-print("Hello routes")
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Configuración de la base de datos
+DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Declaración de la tabla de usuarios
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+
+Base.metadata.create_all(bind=engine)
+
+# Modelos Pydantic
+class UserCreate(BaseModel):
+    username: str
+    email: str
+
+# Creación de la aplicación FastAPI
+app = FastAPI()
+
+# Ruta para registrar un usuario
+@app.post("/users/", response_model=UserCreate)
+def create_user(user: UserCreate):
+    db = SessionLocal()
+    db_user = User(**user.dict())
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    db.close()
+    return user
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
