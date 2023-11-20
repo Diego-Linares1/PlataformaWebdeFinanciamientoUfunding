367ede6d7f2878490da1add0cb22cc16e19eea6c
JoshiParSa <joshiparsa@gmail.com>
2023-11-20 15:04:49 -0500

    Creando LB02

---
+++ Linea Base/PWFU/PWFU-LB02/Codigo/Backend/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI(title="Ufunding")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir a todos los dominios
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permitir todos los encabezados
+)
+
+from app.api.route import router
+app.include_router(router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app,host="127.0.0.1", port=8000)
